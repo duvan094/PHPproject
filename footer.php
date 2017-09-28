67e9b3c9131445412ff7afce4490704eefe3bfae
QueenE1337 <emmaradhall@wlanguest86108.eduroam.hj.se>
2017-09-28 14:45:13 +0200

    Link in footer changes

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
 
-	<p><a href="https://www.facebook.com/jacob.duvander?ref=br_rs">Like us on Facebook</a></p>
+	<p><a href="https://www.facebook.com/jacob.duvander?ref=br_rs" target="_blank">Like us on Facebook</a></p>
 
 	<img src="img/JayD.png">
 

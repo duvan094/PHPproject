decc89711215e6d4c87fe363c2f5d0920627959e
Jacob <jacobduvander@gmail.com>
2017-09-28 13:06:52 +0200

    Added some basic buttons

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
     <?php include "header.php" ?>
     <main>
-      <h1>Would You Rather</h1>
+      <h3>Would You Rather?</h1>
+
+      <ul id="card-container">
+        <li><button>This?</button></li>
+        <li><button>Or This?</button></li>
+      </ul>
     </main>
     <?php  include "footer.php"?>

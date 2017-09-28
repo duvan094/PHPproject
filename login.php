de1d923087fc603077849d7f2363b729d4a5777e
QueenE1337 <emmaradhall@wlanguest86108.eduroam.hj.se>
2017-09-28 15:45:29 +0200

    UserAccount & Login Pop Up

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php include "header.php" ?>
+
+<main>
+
+
+<div id="logincontainer">
+	
+	<h3>Log In</h3>
+
+	<form>
+		<input type="text" name="username"> <br>
+		<input type="password" name="password"> <br>
+		<input type="submit" name="SIGN IN">
+	</form>
+
+	<p>Don't have an account? <a href="">Sign Up</a> for free!</p>
+
+</div>
+   
+
+<?php  include "footer.php"?>
---
+++ useraccount.php
@@ -0,0 +1,25 @@
+<?php include "header.php" ?>
+
+<main>
+
+
+<div id="userprofil">
+
+	<h3>USERNAME</h3>
+
+
+	<ul>
+
+		<li>
+			
+		</li>
+
+	</ul>
+
+</div>
+
+
+</main>
+   
+
+<?php  include "footer.php"?>

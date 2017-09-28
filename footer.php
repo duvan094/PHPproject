93c683e2744e27eb0bbe7a50717330384a6db94c
QueenE1337 <emmaradhall@wlanguest86108.eduroam.hj.se>
2017-09-28 14:53:44 +0200

    footer transitions

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 	<p><a href="https://www.facebook.com/jacob.duvander?ref=br_rs" target="_blank">Like us on Facebook</a></p>
 
-	<img src="img/JayD.png">
+	<a href="https://www.facebook.com/jacob.duvander?ref=br_rs" target="_blank"> <img src="img/JayD.png"> </a>
 
 </footer>
 

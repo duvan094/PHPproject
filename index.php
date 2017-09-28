fcc65dc536812419e6e60bf5e3dfb54a7b3503b4
Jacob <jacobduvander@gmail.com>
2017-09-28 11:43:18 +0200

    added some files

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Would you rather!</title>
-  </head>
-  <body>
+    <?php include "header.php" ?>
     <h1>This is our project!</h1>
-  </body>
-</html>
+
+
+    <?php  include "footer.php"?>

eba9963ecfa2c8ddb1ecc5a23d5bb9319ee62a65
QueenE1337 <emmaradhall@wlanguest18649.eduroam.hj.se>
2017-09-28 11:41:15 +0200

    Created CSS file, linked CSS in HTML file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Would you rather!</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
   </head>
   <body>
     <h1>This is our project!</h1>

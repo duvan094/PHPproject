c4b16b9dd9f0f60d08b6ae4e34b929a8ac3334f7
Swindian95 <mihir.chouhan1@gmail.com>
2017-10-05 15:09:07 +0200

    signup

---
+++ login.php
@@ -0,0 +1,25 @@
+<div id=loginWrapper>
+
+	<div id="logincontainer">
+
+		<h3>Log In</h3>
+
+		<form>
+			<p>USERNAME</p>
+			<input type="text" name="username"> <br>
+			<p>PASSWORD</p>
+			<input type="password" name="password"> <br>
+			<input type="submit" name="SIGN IN" value="SIGN IN">
+		</form>
+		<p class = "textWithLink">Don't have an account? <a href="signup.php">Sign Up</a> for free!</p>
+
+	</div>
+</div>
+
+
+<?php include "header.php" ?>
+
+<main>
+</main>
+
+<?php  include "footer.php"?>

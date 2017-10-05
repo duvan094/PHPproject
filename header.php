594b07092010acd4527b1116bb34b05e305c72a6
Swindian95 <mihir.chouhan1@gmail.com>
2017-10-05 15:30:46 +0200

    fixed

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
       <input type="password" name="password"> <br>
       <input type="submit" name="SIGN IN" value="SIGN IN">
     </form>
-    <p class = "textWithLink">Don't have an account? <a href="">Sign Up</a> for free!</p>
+    <p class = "textWithLink">Don't have an account? <a href="" id="signupShow">Sign Up</a> for free!</p>
 
   </div>
 </div>

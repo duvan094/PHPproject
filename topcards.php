e0f71d92a356ac1547bd5992c9f72adcb23934ca
Jacob <jacobduvander@gmail.com>
2017-10-17 21:52:10 +0200

    Styled the topList a bit better

--- topcards.php
+++ topcards.php
@@ -1,26 +1,75 @@
 <?php include "header.php" ?>
 
-<main>
+<main id="topListContainer">
 
-	<h2>Our Most Popular Cards</h2>
-
-	<table>
+	<h1>Top 10</h1>
+	<table id="topList">
 		<tr>
+			<th>Rank</th>
 			<th>Title</th>
 			<th>Author</th>
 			<th>Rating</th>
 		</tr>
-		<ol>
 		<tr>
-    		<td>Hitler without a mustasche</td>
-    		<td>Buksmilet420</td>
+				<td>1</td>
+    		<td><a href="#">Hitler without a mustasche</a></td>
+    		<td><a href="#">Buksmilet420</a></td>
     		<td>346</td>
   		</tr>
   		<tr>
-    		<td>Exploding kittens or dogs</td>
-    		<td>Oatmeal</td>
+				<td>2</td>
+    		<td><a href="#">Exploding kittens or dogs</a></td>
+    		<td><a href="#">Oatmeal</a></td>
     		<td>263</td>
   		</tr>
+			<tr>
+				<td>3</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
+			<tr>
+				<td>6</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
+			<tr>
+				<td>7</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
+			<tr>
+				<td>8</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
+			<tr>
+				<td>9</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
+			<tr>
+				<td>10</td>
+				<td><a href="#">Milk or Beer</a></td>
+				<td><a href="#">Steffe82</a></td>
+				<td>201</td>
+			</tr>
 	</table>
 
 

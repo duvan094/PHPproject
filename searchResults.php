fffdd8a9031f0ed39ea3f5f87ea64dd72c878381
Jacob <jacobduvander@gmail.com>
2017-10-20 14:56:30 +0200

    Search now kinda works

--- searchResults.php
+++ searchResults.php
@@ -1,14 +1,50 @@
 <?php include "header.php" ?>
 <main id="searchPage">
   <div id="searchMenu">
+    <?php
+      @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+      /*Check for connection error*/
+      if ($db->connect_error) {
+          echo "could not connect: " . $db->connect_error;
+          printf("<br><a href=index.php>Return to home page </a>");
+          exit();
+      }
+
+        $query = "select * from ListCards Where title LIKE'%" .  $_GET['searchField'] . "%' OR alt1 LIKE'%" .  $_GET['searchField'] . "%' OR alt2 LIKE'%" .  $_GET['searchField'] . "%' OR username LIKE'%" .  $_GET['searchField'] . "%'";
+        $stmt = $db->prepare($query);
+        $stmt->bind_result($title, $alt1, $alt2, $alt1Count, $alt2Count, $rating, $categoryName,$username,$dateAdded);
+        $stmt->execute();
+        $stmt->store_result();
+        $nbrOfResults = $stmt->num_rows();
+    ?>
+
     <div class="searchMenuWrapper">
       <button type="button" name="button">Cards</button>
       <button type="button" name="button">Users</button>
-      <p>3 card results for <i>Nyan Cat</i></p>
+      <p><?php echo $nbrOfResults ?> card results for <i><?php echo $_GET['searchField'] ?></i></p>
     </div>
   </div>
   <ul>
-    <li>
+    <?php
+      while($stmt->fetch()){
+       echo "<li>";
+       echo "<h3>Would You Rather</h3>";
+       echo "<ul class='card-container'>";
+       echo "<li><button>$alt1</button></li>";
+       echo "<li><button>$alt2</button></li>";
+       echo "</ul>";
+       echo "<ul class='upvote-container'>";
+       echo "<li><button class='like-button'><i class='fa fa-thumbs-o-down' aria-hidden='true'></i></button></li>";
+       echo "<li><p>$rating</p></li>";
+       echo "<li><button class='like-button'><i class='fa fa-thumbs-o-up' aria-hidden='true'></i></i></button></li>";
+       echo "</ul>";
+       echo "<p class='textWithLink'>Made by <a href=''>$username</a>, $dateAdded</p>";
+       echo "</li>";
+      }
+
+    ?>
+    <!--<li>
       <h3>If You Where A Cat Would You Do...</h3>
       <ul class="card-container">
         <li><button>This?</button></li>
@@ -46,7 +82,7 @@
         <li><button class="like-button"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></i></button></li>
       </ul>
       <p class="textWithLink">Made by <a href="#">joppeBoi</a>, 32 days ago.</p>
-    </li>
+    </li>-->
   </ul>
 </main>
 <?php include "footer.php" ?>

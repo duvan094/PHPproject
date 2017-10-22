78085867d331f771bbcb6917f477c6f8d92c46c8
Jacob <jacobduvander@gmail.com>
2017-10-22 11:38:26 +0200

    Added min-height for the page in the CSS
    Now there is a min-height for the page. Which prevents pages without
    much content from looking as silly as previously when the footer used to
    be very high up on the page.

--- profile.php
+++ profile.php
@@ -40,7 +40,7 @@
 						echo "<li><p>$rating</p></li>";
 						echo "<li><button class='like-button'><i class='fa fa-thumbs-o-up' aria-hidden='true'></i></i></button></li>";
 						echo "</ul>";
-						echo "<p class='textWithLink'><a href='index.php?cardId=$cardId'>$title</a>made by <a href='profile.php?username=$username'>$username</a>, $dateAdded</p>";
+						echo "<p class='textWithLink'><a href='index.php?cardId=$cardId'>$title</a> made by <a href='profile.php?username=$username'>$username</a>, $dateAdded</p>";
 						echo "</li>";
 					}
 					echo "</ul>";

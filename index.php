84f19427257e17627fb0a9406d9196cdd6fcbd97
Jacob <jacobduvander@gmail.com>
2017-10-23 14:33:03 +0200

    Added categories buttons

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
     <?php include "header.php" ?>
     <main>
+
+
       <h1>Would You Rather?</h1>
     <?php
 
@@ -69,6 +71,7 @@
         <li><a href="index.php?cardId=<?php echo $cardId+1;?>">Next Question</a></li>
       </ul>
 
+
       <?php
 
         @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
@@ -109,6 +112,28 @@
       </li>
     </ul>
 
+    <?php
+      @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+      /*Check for connection error*/
+      if ($db->connect_error) {
+        echo "could not connect: " . $db->connect_error;
+        printf("<br><a href=index.php>Return to home page </a>");
+        exit();
+      }
+
+      $query = "select * from Categories";
+      $stmt = $db->prepare($query);
+      $stmt->bind_result($categoryId,$categoryName);
+      $stmt->execute();
+
+      echo "<ul class='categoriesList'>";
+      while ($stmt->fetch()) {
+        echo "<li><a href='index.php?categoryName=$categoryName'>$categoryName</a></li>";
+      }
+      echo "</ul>";
+
+    ?>
       <!--<ul id="commentContainer">
         <li><h4>4 comments</h4></li>
         <li class="commentField">

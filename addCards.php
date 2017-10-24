bfc023c25fcf97cca3b656875ba599820caeb7c2
QueenE1337 <emmaradhall@wlanguest14369.eduroam.hj.se>
2017-10-24 14:23:45 +0200

    AddCards styling

--- addCards.php
+++ addCards.php
@@ -1,9 +1,28 @@
 <?php include "header.php" ?>
 
 <main>
+
   <?php
     if(!isset($_SESSION['username'])) {//Only include loginModule if logged in.
       echo "<h3>You have to Log in to create cards.</h3>";
+
+    } else {
+
+      echo "<div class='addCardsContainer'>";
+      echo "<h3>Here you create your own cards.</h3>";
+      echo "<p>Please fill out a title with maximum 30 caracters, and two Would You Rather-alternatives.</p><br>";
+
+      echo "<h3>Title:</h3>";
+      echo "<form><input type='text' name='title'></form> <br>";
+
+      echo "<h3>Would you rather?</h3>";
+      echo "<ul class='card-container'>";
+      echo "<li> <form class='card'> <input id='addCardsInput' type='text' name='alt1' placeholder='Alternative One'> </form> </li>";
+      echo "<li> <form class='card'> <input id='addCardsInput' type='text' name='alt2' placeholder='Alternative Two'> </form> </li>";
+
+      echo "</ul>";
+      echo "</div>";
+
     }
   ?>
 

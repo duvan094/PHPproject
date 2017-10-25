11bcbf83823cecdb9458341cc9458eb7c618fd43
Jacob <jacobduvander@gmail.com>
2017-10-25 22:16:50 +0200

    removing unecessary code :+1:

--- profile.php
+++ profile.php
@@ -53,52 +53,6 @@
 		}
 	?>
 
-<!--
-	<h1>steffe94</h1>
-	<ul>
-
-		<li>
-			<h3>If You Where A Cat Would You Do...</h3>
-			<ul class="card-container">
-        <li><button>This?</button></li>
-        <li><button>That?</button></li>
-      </ul>
-			<ul class="upvote-container">
-				<li><button class="like-button"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button></li>
-				<li><p>0</p></li>
-				<li><button class="like-button"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></i></button></li>
-			</ul>
-			<p class="textWithLink">Made by <a href="#">steffe94</a>, 45 days ago.</p>
-		</li>
-		<li>
-			<h3>Would You Rather Do...</h3>
-			<ul class="card-container">
-        <li><button>This?</button></li>
-        <li><button>That?</button></li>
-      </ul>
-			<ul class="upvote-container">
-				<li><button class="like-button"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button></li>
-				<li><p>0</p></li>
-				<li><button class="like-button"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></i></button></li>
-			</ul>
-			<p class="textWithLink">Made by <a href="#">steffe94</a>, 2 days ago.</p>
-		</li>
-		<li>
-			<h3>Would You Rather Do...</h3>
-			<ul class="card-container">
-				<li><button>This?</button></li>
-				<li><button>That?</button></li>
-			</ul>
-			<ul class="upvote-container">
-				<li><button class="like-button"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></button></li>
-				<li><p>0</p></li>
-				<li><button class="like-button"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i></i></button></li>
-			</ul>
-			<p class="textWithLink">Made by <a href="#">steffe94</a>, 32 days ago.</p>
-		</li>
-	</ul>
--->
-
 </main>
 
 

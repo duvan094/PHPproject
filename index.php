9fe28bf8eb5cf3e094dc5bccd65658361faf5bf4
Jacob <jacobduvander@gmail.com>
2017-10-26 00:57:18 +0200

    Started working on a different CSS scheme

--- index.php
+++ index.php
@@ -93,21 +93,27 @@
       echo "</form>";
     }
 
-    echo "<ul class='upvote-container'>";
-    echo "<li><button class='like-button'><i class='fa fa-thumbs-o-down' aria-hidden='true'></i></button></li>";
-    echo "<li><p>$rating</p></li>";
-    echo "<li><button class='like-button'><i class='fa fa-thumbs-o-up' aria-hidden='true'></i></i></button></li>";
-    echo  "</ul>";
-    echo  "<p class='textWithLink'><a href='index.php?cardId=$cardId'>$title</a> made by <a href='profile.php?username=$username'>$username</a>, $dateAdded</p>";
-
-    $cardIdGlobal = $cardId;/*Save the cardId in a global variable*/
-
   ?>
 
   <ul id="nextPrevButtons">
     <li><a href="index.php?cardId=<?php echo $cardId-1;?>">Previous Question</a></li>
     <li><a href="index.php?cardId=<?php echo $cardId+1;?>">Next Question</a></li>
   </ul>
+  <div class="container">
+  <div>
+  <?php
+    echo "<ul class='upvote-container'>";
+    echo "<li><button class='like-button'><i class='fa fa-thumbs-down' aria-hidden='true'></i></i></button></li>";
+    echo "<li><p>$rating</p></li>";
+    echo "<li><button class='like-button'><i class='fa fa-thumbs-up' aria-hidden='true'></i></button></li>";
+    echo  "</ul>";
+    echo  "<p class='textWithLink'><a class='cardLinkTitle' href='index.php?cardId=$cardId'>$title</a><br>Made by <i class='fa fa-user' aria-hidden='true'></i> <a href='profile.php?username=$username'>$username</a>, $dateAdded</p>";
+
+    $cardIdGlobal = $cardId;/*Save the cardId in a global variable*/
+
+  ?>
+
+
 
   <?php
     /*Connect to the database to insert the comment*/
@@ -199,7 +205,8 @@
     echo "</ul>";
 
   ?>
-
+  </div>
+  </div>
 </main>
 
 

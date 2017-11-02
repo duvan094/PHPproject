5f24756fba7846df053b6ec915618752f98b8efb
Jacob <jacobduvander@gmail.com>
2017-11-02 11:30:59 +0100

    Session hijacking prevention

--- header.php
+++ header.php
@@ -1,5 +1,22 @@
 <?php
-session_start();
+ini_set('session.cookie_httponly', true);
+
+session_start ();
+
+if (isset($_SESSION['userip']) === false){
+
+    #here we store the IP into the session 'userip'
+    $_SESSION['userip'] = $_SERVER['REMOTE_ADDR'];
+}
+
+if ($_SESSION['userip'] !== $_SERVER['REMOTE_ADDR']){
+    #if it is not the same, we just remove all session variables
+    #this way the attacker will have no session
+    session_unset();
+    session_destroy();
+
+}
+
 ?>
 
 <?php include "config.php" ?>

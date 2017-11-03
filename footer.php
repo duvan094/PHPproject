f320f3514b568657cc0f6c863f47e252aab98ce0
Jacob <jacobduvander@gmail.com>
2017-11-03 11:47:41 +0100

    Small change

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<form id='colorToggle' action='' method='post'>
-		<input type="submit" name="changeColor" value="Change Color">
+		<input type="submit" name="changeColor" value="Change Color Theme">
 	</form>
 
 	<p><a href="https://www.facebook.com/jacob.duvander?ref=br_rs" target="_blank">Life isn't about finding yourself. Life is about creating yourself.</a></p>

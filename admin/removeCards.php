2a42902998e807f553b8bae08f4c113580aea0da
Jacob <jacobduvander@gmail.com>
2017-11-07 18:08:25 +0100

    Fixed bug

--- admin/removeCards.php
+++ admin/removeCards.php
@@ -20,6 +20,16 @@
     $stmt = $db->prepare("Delete From Comments Where cardId=$cardId");
     $stmt->execute();
 
+    @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+      if ($db->connect_error) {
+          echo "could not connect: " . $db->connect_error;
+          printf("<br><a href=index.php>Return to home page </a>");
+          exit();
+      }
+
+      $stmt = $db->prepare("Delete From CardsUsersRating Where cardId=$cardId");
+      $stmt->execute();
 
     if ($db->connect_error) {
         echo "could not connect: " . $db->connect_error;

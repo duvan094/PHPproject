3e20e354d0c24f2a0ddcfb50b80ba68a44544a23
Swindian95 <mihir.chouhan1@gmail.com>
2017-11-08 14:58:57 +0100

    fixed css for delete card

--- removeOwnCard.php
+++ removeOwnCard.php
@@ -26,7 +26,7 @@
     $stmt = $db->prepare("DELETE FROM Cards WHERE cardId=$cardId");
     $stmt->execute();
 
-    header("location: profile.php?");
+    header("location: profile.php?username=$_SESSION['username']");
     exit;
 
 ?>

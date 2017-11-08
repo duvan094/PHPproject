839ba80044ef66f4bbf3948a2786a9e1bbcc81c1
Jacob <jacobduvander@gmail.com>
2017-11-08 15:07:33 +0100

    fixed error on profile

--- removeOwnCard.php
+++ removeOwnCard.php
@@ -16,6 +16,17 @@
     $stmt = $db->prepare("Delete From Comments Where cardId=$cardId");
     $stmt->execute();
 
+    @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+
+      if ($db->connect_error) {
+          echo "could not connect: " . $db->connect_error;
+          printf("<br><a href=index.php>Return to home page </a>");
+          exit();
+      }
+
+      $stmt = $db->prepare("Delete From CardsUsersRating Where cardId=$cardId");
+      $stmt->execute();
+
 
     if ($db->connect_error) {
         echo "could not connect: " . $db->connect_error;
@@ -23,10 +34,10 @@
         exit();
     }
 
-    $stmt = $db->prepare("DELETE FROM Cards WHERE cardId=$cardId");
+    $stmt = $db->prepare("Delete From Cards Where cardId=$cardId");
     $stmt->execute();
 
-    header("location: profile.php?username=$_SESSION['username']");
+    header("location: profile.php?username={$_SESSION['username']}");
     exit;
 
 ?>

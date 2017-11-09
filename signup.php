6cf9f2d5fc8ae30de25baa84780e0077eaa3cd22
Jacob <jacobduvander@gmail.com>
2017-11-09 14:42:10 +0100

    Fixed so that users can't add same username twice

--- signup.php
+++ signup.php
@@ -6,44 +6,60 @@
 
 		@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
-   	if ($db->connect_error) {
-	    echo "could not connect: " . $db->connect_error;
-	    printf("<br><a href=index.php>Return to home page </a>");
-    	exit();
-	  }
+		if ($db->connect_error) {
+			echo "could not connect: " . $db->connect_error;
+			printf("<br><a href=index.php>Return to home page </a>");
+			exit();
+		}
 
-    $username = mysqli_real_escape_string($db,$_POST['username']);
-    $password = mysqli_real_escape_string($db,$_POST['password1']);
-		$password = SHA1($password);
-    $countryId = ($_POST['countrySelect']);
+		/*Check if username already exists in the database*/
+		$stmt = $db->prepare("select * From Users Where username = '{$_POST['username']}'");
+		$stmt->bind_result($userId,$username,$password,$country);
+		$stmt->execute();
+		$stmt->store_result();
 
-    if(isset($_POST['countrySelect']) && $_POST['countrySelect'] !== ""){
-      $query = "INSERT INTO users(username, password, countryId) VALUES('{$username}','{$password}',{$countryId})";
-    }else{
-      $query = "INSERT INTO users(username, password, countryId) VALUES('{$username}','{$password}', NULL)";
-    }
+		if($stmt->num_rows() == 0){/*If the username don't exist it should return 0 rows.*/
+			@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
-    $stmt = $db->prepare($query);
-    $stmt->execute();
+	   	if ($db->connect_error) {
+		    echo "could not connect: " . $db->connect_error;
+		    printf("<br><a href=index.php>Return to home page </a>");
+	    	exit();
+		  }
 
-    @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+	    $username = mysqli_real_escape_string($db,$_POST['username']);
+	    $password = mysqli_real_escape_string($db,$_POST['password1']);
+			$password = SHA1($password);
+	    $countryId = ($_POST['countrySelect']);
 
-    if ($db->connect_error) {
-      echo "could not connect: " . $db->connect_error;
-      printf("<br><a href=index.php>Return to home page </a>");
-     	exit();
-    }
+	    if(isset($_POST['countrySelect']) && $_POST['countrySelect'] !== ""){
+	      $query = "INSERT INTO users(username, password, countryId) VALUES('{$username}','{$password}',{$countryId})";
+	    }else{
+	      $query = "INSERT INTO users(username, password, countryId) VALUES('{$username}','{$password}', NULL)";
+	    }
 
-    $query = "select userId From Users Where username = '{$username}'";
+	    $stmt = $db->prepare($query);
+	    $stmt->execute();
 
-    $stmt = $db->prepare("$query");
-    $stmt->bind_result($userId);
-    $stmt->execute();
-		$stmt->fetch();
+	    @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
-    session_start();
-    $_SESSION['username'] = $username;
-    $_SESSION['userId'] = $userId;
+	    if ($db->connect_error) {
+	      echo "could not connect: " . $db->connect_error;
+	      printf("<br><a href=index.php>Return to home page </a>");
+	     	exit();
+	    }
+
+	    $query = "select userId From Users Where username = '{$username}'";
+
+	    $stmt = $db->prepare("$query");
+	    $stmt->bind_result($userId);
+	    $stmt->execute();
+			$stmt->fetch();
+
+	    session_start();
+	    $_SESSION['username'] = $username;
+	    $_SESSION['userId'] = $userId;
+		}
 	}
-	header("location: index.php")
+	header("location: index.php");
 ?>
